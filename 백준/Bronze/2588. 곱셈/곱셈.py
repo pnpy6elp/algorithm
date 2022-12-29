a0838d28b4a03841f66a6595aa5eb3784e2a1ed7
Bongbong <51867651+pnpy6elp@users.noreply.github.com>
2022-12-29 16:47:37 +0900

    Update 곱셈.py

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -2,6 +2,7 @@ data = [int(input()) for i in range(2)]
 data[1] = str(data[1])
 result=[]
 zero = 0
+# 문자열 reverse : list(문자열변수[-1::-1])
 for i in list(data[1][-1::-1]):
     result.append(data[0]*int(i+"0"*zero))
     print(data[0]*int(i))
