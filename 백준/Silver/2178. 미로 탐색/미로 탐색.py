a185a2cb774a4efbb5629b173c081820600978c3
Bong Bong <51867651+pnpy6elp@users.noreply.github.com>
2025-09-28 22:48:44 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,22 +1,28 @@
-from collections import deque
 import sys
+from collections import deque
+N, M = map(int, sys.stdin.readline().split())
+graph = []
+for i in range(N):
+    graph.append(list(map(int, sys.stdin.readline().strip())))
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+def bfs(x, y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx <0 or ny <0 or nx >= N or ny >= M:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+                
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+    return graph[N-1][M-1]
 
-# 인접행렬(상하좌우) -> BFS
-N, M = map(int, input().split()) # N: 세로, M: 가로
-MAP = [[int(i) for i in list(input())] for _ in range(N)]
-check = [[0] * M for _ in range(N)] # visited
-dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
-q = deque() # bfs에서는 queue
-q.append([0,0])
-check[0][0] = 1
-while q:
-    x, y = q.popleft()
-    for i in range(4): # 상하좌우
-        nx, ny = x + dx[i], y + dy[i] # 방향설정..?
-        if 0 <= nx < N and 0 <= ny < M and check[nx][ny] ==0 and MAP[nx][ny] == 1:
-            q.append([nx,ny])
-            check[nx][ny] = check[x][y] + 1
-print(check[-1][-1])
-
-
+print(bfs(0,0))
+    
