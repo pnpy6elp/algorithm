867f1bb9e597cdf2526b52f595dd4e774df966cf
Bong Bong <51867651+pnpy6elp@users.noreply.github.com>
2025-11-02 10:34:31 +0900

    [level 2] Title: 타겟 넘버, Time: 378.80 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,17 @@
+answer = 0
+def dfs(numbers, target, current, idx):
+    global answer
+    
+    if idx == len(numbers):
+        if target == current:
+            answer += 1
+        return
+    dfs(numbers, target, current + numbers[idx], idx+1)
+    dfs(numbers, target, current - numbers[idx], idx+1)
+    
+    
+
+
+def solution(numbers, target):
+    dfs(numbers, target, 0, 0)
+    return answer
