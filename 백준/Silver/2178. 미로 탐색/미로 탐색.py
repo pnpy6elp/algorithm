cfdbb1061ac8ce3d98a81c565e9072f3f67a77ba
Bong Bong <51867651+pnpy6elp@users.noreply.github.com>
2026-03-25 23:05:17 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,28 +1,42 @@
 import sys
 from collections import deque
-N, M = map(int, sys.stdin.readline().split())
-graph = []
-for i in range(N):
-    graph.append(list(map(int, sys.stdin.readline().strip())))
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-def bfs(x, y):
-    queue = deque()
-    queue.append((x,y))
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx <0 or ny <0 or nx >= N or ny >= M:
-                continue
-            if graph[nx][ny] == 0:
-                continue
-                
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx,ny))
-    return graph[N-1][M-1]
+input = sys.stdin.readline
+# 첫째 줄에 두 정수 N, M(2 ≤ N, M ≤ 100)이 주어진다. 
+n, m = map(int, input().split())
+# 다음 N개의 줄에는 M개의 정수로 미로가 주어진다. 각각의 수들은 붙어서 입력으로 주어진다.
+graph = [list(map(int, input().strip())) for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
+cnt_li = [[0] * m for _ in range(n)]
 
-print(bfs(0,0))
+def in_range(x , y):
+    return (0 <= x and x < n and 0 <= y and y <m)
+def can_go(x, y):
+    return in_range(x, y) and graph[x][y] and not visited[x][y]
+
+
+def bfs():
+    q = deque([(0,0)])
+    visited[0][0] = True
+    cnt_li[0][0] = 1 # 시작점의 거리는 1로 시작
     
+    while q:
+        x, y = q.popleft()
+        dxs, dys = [0,1,0,-1], [1,0,-1,0]
+        
+        for dx, dy in zip(dxs, dys):
+            point_x, point_y = x + dx, y + dy
+            
+            if can_go(point_x, point_y):
+                # 1. 큐에 넣기 전에 무조건 방문 처리! (메모리 초과 방지)
+                visited[point_x][point_y] = True
+                
+                # 2. 내 거리는 '직전 칸의 거리 + 1' (최단 거리 계산)
+                cnt_li[point_x][point_y] = cnt_li[x][y] + 1
+                
+                q.append((point_x, point_y))
+bfs()
+chk = 1 if visited[n - 1][m - 1] else 0
+if chk:
+    print(cnt_li[n - 1][m - 1])
+else:
+    print()
