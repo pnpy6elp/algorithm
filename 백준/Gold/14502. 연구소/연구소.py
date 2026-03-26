b78222a2bae46e4f05950456f94f402b7fc925d0
Bong Bong <51867651+pnpy6elp@users.noreply.github.com>
2026-03-26 17:40:12 +0900

    [Gold IV] Title: 연구소, Time: 4176 ms, Memory: 37076 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,67 +1,55 @@
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-# 0: 빈칸, 1: 벽, 2: 바이러스
-# 완전 탐색 해도 괜찮려나?
-# 1을 추가로 3개 더 채웠을 때 0의 최대 크기
-from collections import deque
-def in_range(x, y):
-    return (0<=x<n) and (0<=y<m)
-
-dxs, dys = [0,1,0,-1], [1,0,-1,0]
-
-
-def comb(arr, n):
-    res = []
-    if n==0:
-        return [[]]
-    
-    for i in range(0, len(arr)):
-        elem = arr[i]
-        rest = arr[i+1:]
-        for c in comb(rest, n-1):
-            res.append([elem]+c)
-    return res
-
 blank = []
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 0:
             blank.append((i,j))
+            
+def comb(arr, n):
+    res = []
+    if n == 0:
+        return [[]]
+    for i in range(0, len(arr)):
+        first = arr[i]
+        rest = arr[i+1:]
+        for k in comb(rest, n-1):
+            res.append([first]+k)
+    return res
 
-wall_comb = comb(blank,3)
-
-visited = [[False]*m for _ in range(n)]
-
-def bfs(q,graph):
-    while q:
-        x, y = q.popleft()
-        for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy
-            if in_range(nx,ny) and graph[nx][ny]==0:
-                graph[nx][ny] = 2
-                q.append((nx,ny))
-    return graph
-
+# 2 바이러스, 1 벽, 0 빈칸
+from collections import deque
+dxs, dys = [0,1,0,-1], [1,0,-1,0]
+walls = comb(blank, 3)
 cnt_li = []
-for C in wall_comb:
+for wall in walls:
     new_graph = [arr[:] for arr in graph]
-    for w in C:
+    for w in wall:
         new_graph[w[0]][w[1]] = 1
     q = deque([])
+    # 바이러스 출발점 여러개니까 큐에 다 넣기
     for i in range(n):
         for j in range(m):
-            if graph[i][j] == 2:
+            if new_graph[i][j]==2:
                 q.append((i,j))
-    new_graph = bfs(q, new_graph)
-    
-    visited = [[False]*m for _ in range(n)]
+                
+    while q:
+        x, y = q.popleft()
+        
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if (0<=nx<n) and (0<=ny<m) and new_graph[nx][ny]==0:
+                new_graph[nx][ny] = 2
+                q.append((nx,ny))
     cnt = 0
     for i in range(n):
         for j in range(m):
-            if new_graph[i][j]==0:
+            if new_graph[i][j] == 0:
                 cnt += 1
     cnt_li.append(cnt)
-                
 print(max(cnt_li))
-
+        
+    
+    
+        
