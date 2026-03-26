c4cac33940f1481889f43a56d0fe7963f5c6550a
Bong Bong <51867651+pnpy6elp@users.noreply.github.com>
2026-03-27 03:15:05 +0900

    [Gold III] Title: 아기 상어, Time: 180 ms, Memory: 35044 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,84 +1,64 @@
-n = int(input())
-# 정사각형
-mat = [list(map(int, input().split())) for _ in range(n)]
-# 아기상어 위치
+from collections import deque
 
+n = int(input())
+mat = [list(map(int, input().split())) for _ in range(n)]
+
+# 아기 상어 초기 위치 찾기
 for i in range(n):
     for j in range(n):
         if mat[i][j] == 9:
             baby_x, baby_y = i, j
             mat[i][j] = 0
-            
-from collections import deque
 
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
 
-dxs, dys = [0,1,0,-1], [1,0,-1,0]
-
-def bfs(q,visited,size):
+def bfs(bx, by, size):
+    q = deque([(bx, by)])
+    dist = [[-1] * n for _ in range(n)]
+    dist[bx][by] = 0
+    
     while q:
         x, y = q.popleft()
         for dx, dy in zip(dxs, dys):
             nx, ny = x + dx, y + dy
-            if 0<=nx<n and 0<=ny<n and visited[nx][ny]==-1 and size >= mat[nx][ny]:
-                visited[nx][ny] = visited[x][y] + 1
-                q.append((nx,ny))
-    return visited
+            # 갈 수 있는 곳(빈칸이거나 크기가 작거나 같은 물고기)
+            if 0 <= nx < n and 0 <= ny < n and dist[nx][ny] == -1 and size >= mat[nx][ny]:
+                dist[nx][ny] = dist[x][y] + 1
+                q.append((nx, ny))
+    return dist
 
 size = 2
 time = 0
 current_feed = 0
 
 while True:
-    q = deque([(baby_x, baby_y)]) # 현 위치에서 출발
-    dist = [[-1]*n for _ in range(n)]
-    dist[baby_x][baby_y] = 0
-    dist = bfs(q,dist,size)
+    dist = bfs(baby_x, baby_y, size)
     fish = []
+    
+    # 먹을 수 있는 물고기 탐색
     for i in range(n):
         for j in range(n):
             if 0 < mat[i][j] < size and dist[i][j] != -1:
-                fish.append([(i,j),dist[i][j]])
-    if len(fish) == 0:
+                # (거리, x좌표, y좌표) 순서로 튜플 저장!!!
+                fish.append((dist[i][j], i, j))
+                
+    # 더 이상 먹을 물고기가 없으면 종료
+    if not fish:
         print(time)
-        exit()
-    elif len(fish) == 1:
-        baby_x, baby_y = fish[0][0]
-        mat[baby_x][baby_y] = 0
-        time += fish[0][1]
-        current_feed += 1
-        if current_feed == size:
-            size += 1
-            current_feed = 0
-    else:
-        fish.sort(key=lambda x:x[1])
-        min_dist = fish[0][1]
-        new_fish = [f for f in fish if f[1]==min_dist]
-        if len(new_fish)==1:
-            baby_x, baby_y = new_fish[0][0]
-            mat[baby_x][baby_y] = 0
-            time += new_fish[0][1]
-            current_feed += 1
-            if current_feed == size:
-                size += 1
-                current_feed = 0
-        else:
-            new_fish.sort(key=lambda x:x[0][0])
-            min_x = new_fish[0][0][0]
-            new_fish = [f for f in new_fish if f[0][0]==min_x]
-            if len(new_fish)==1:
-                baby_x, baby_y = new_fish[0][0]
-                mat[baby_x][baby_y] = 0
-                time += new_fish[0][1]
-                current_feed += 1
-                if current_feed == size:
-                    size += 1
-                    current_feed = 0
-            else:
-                new_fish.sort(key=lambda x:x[0][1])
-                baby_x, baby_y = new_fish[0][0]
-                mat[baby_x][baby_y] = 0
-                time += new_fish[0][1]
-                current_feed += 1
-                if current_feed == size:
-                    size += 1
-                    current_feed = 0
+        break
+        
+    # 거리 -> x(위) -> y(왼쪽) 순으로 자동 오름차순 정렬됨
+    fish.sort()
+    
+    # 정렬된 리스트의 첫 번째(0번 인덱스)가 무조건 타겟!
+    min_dist, target_x, target_y = fish[0]
+    
+    # 상어 이동 및 먹기 처리
+    baby_x, baby_y = target_x, target_y
+    mat[baby_x][baby_y] = 0
+    time += min_dist
+    current_feed += 1
+    
+    if current_feed == size:
+        size += 1
+        current_feed = 0
